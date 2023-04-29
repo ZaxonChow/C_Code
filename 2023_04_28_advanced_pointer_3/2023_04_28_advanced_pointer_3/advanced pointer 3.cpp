5c7480e7d167511c44d6e893630b6cde6d793367
ZaxonChow <saxon12@126.com>
2023-04-29 11:09:46 +0800

    pointer3

--- 2023_04_28_advanced_pointer_3/2023_04_28_advanced_pointer_3/advanced pointer 3.cpp
+++ 2023_04_28_advanced_pointer_3/2023_04_28_advanced_pointer_3/advanced pointer 3.cpp
@@ -34,3 +34,73 @@ void (*signal(int, void(*)(int)))(int);
 typedef void(* pfun_t)(int);
 
 pfun_t signal(int, pfun_t);
+
+//int Add(int x, int y)
+//{
+//	int z = 0;
+//	z = x + y;
+//	return z;
+//}
+//
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int(*pa)(int, int) = Add;
+//	printf("%d\n", (pa)(2, 3));//
+//	printf("%d\n", Add(2, 3));
+//
+//	printf("%d\n", (*pa)(2, 3));//函数指针 调用时*意义不大
+//	
+//	return 0;
+//}
+
+//int Add(int x, int y)
+//{
+//	return x + y;
+//}
+//
+//int Sub(int x, int y)
+//{
+//	return x - y;
+//}
+//
+//int Mul(int x, int y)
+//{
+//	return x * y;
+//}
+//
+//int Div(int x, int y)
+//{
+//	return x / y;
+//}
+//
+//int main()
+//{
+//	//指针数组
+//	int *arr[5];
+//	//需要一个数组，这个数组可以存放4个函数的地址 - 函数指针的数组
+//	int (*pa)(int, int) = Add;
+//	//函数指针数组
+//	int(*parr[4])(int, int) = { Add, Sub, Mul, Div };
+//	int i = 0;
+//	for (i = 0; i < 4; i++)
+//	{
+//		printf("%d\n", parr[i](4, 2));
+//	}
+//	return 0;
+//}
+
+char* my_strcpy(char* dest, const char* src)
+{
+	return dest;
+}
+//写一个函数指针pf，能够指向my_strcpy
+//写一个函数指针数组pfArr,能够存放4个my_strcpy函数的地址
+
+int main()
+{
+	char* (*pf)(char*, const char*) = my_strcpy;
+	char* (*pfArr[4])(char*, const char*) = { my_strcpy };
+	return 0;
+}
