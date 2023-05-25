61281d0a030e0521987c1f9c8a5118d4f1c336b1
ZaxonChow <saxon12@126.com>
2023-05-25 20:30:19 +0800

    modify

--- 2023_04_05/exercise3.c
+++ 2023_04_05/exercise3.c
@@ -10,7 +10,7 @@ int main()
 	//shutdown -s -t 60
 	//system() - 执行系统命令
 	char input[50] = { 0 };
-	system("shutdown -s -t 30");
+	//system("shutdown -s -t 30");
 again:
 	printf("请注意，您的电脑将在 30s 内关机，如果输入：猛超很帅，就取消关机\n请输入：");
 	scanf("%s", input);
@@ -64,7 +64,7 @@ void game()
 		{
 			printf("恭喜你答对了！\n");
 		}
-	} while (num != ret);	
+	} while (num != ret);
 }
 
 int main()
@@ -98,62 +98,62 @@ int main()
 	return 0;
 }
 */
-	////乘法口诀表
-	//int i = 0, j = 0;
-	//for (i = 1; i <= 9; i++)
-	//{
-	//	for (j = 1; j <= i; j++)
-	//	{
-	//		printf("%d * %d = %-2d  ", i, j, i*j);
-	//	}
-	//	printf("\n");
-	//}
-	//return 0;
-	//int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-	//int max = arr[0];//最大值
-	//int i = 0;
-	//int sz = sizeof(arr) / sizeof(arr[0]); //几个元素
-	//for (i = 0; i < sz; i++)
-	//{
-	//	if (arr[i] > max)
-	//	{
-	//		max = arr[i];
-	//	}
-	//}
-	//printf("max = %d\n", max);
-	//return 0;
-	/*int i = 0;
-	float sum = 0;
-	int flag = 1;
-	for (i = 1; i <= 100; i++)
+////乘法口诀表
+//int i = 0, j = 0;
+//for (i = 1; i <= 9; i++)
+//{
+//	for (j = 1; j <= i; j++)
+//	{
+//		printf("%d * %d = %-2d  ", i, j, i*j);
+//	}
+//	printf("\n");
+//}
+//return 0;
+//int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+//int max = arr[0];//最大值
+//int i = 0;
+//int sz = sizeof(arr) / sizeof(arr[0]); //几个元素
+//for (i = 0; i < sz; i++)
+//{
+//	if (arr[i] > max)
+//	{
+//		max = arr[i];
+//	}
+//}
+//printf("max = %d\n", max);
+//return 0;
+/*int i = 0;
+float sum = 0;
+int flag = 1;
+for (i = 1; i <= 100; i++)
+{
+	sum += flag * 1.0 / i;
+	flag = -flag;
+}
+printf("%lf\n", sum);
+return 0;*/
+/*int i = 0;
+int count = 0;
+for (i = 1; i <= 100; i++)
+{
+	if (i%10 == 9)
+		count++;
+	if (i / 10 == 9)
+		count++;
+}
+printf("%d\n", count);
+return 0;*/
+/*int a = 0, b = 0;
+for (a = 1, b = 1; a <= 100; a++)
+{
+	if (b >= 20) break;
+	if (b % 3 == 1)
 	{
-		sum += flag * 1.0 / i;
-		flag = -flag;
+		b = b + 3;
+		continue;
 	}
-	printf("%lf\n", sum);
-	return 0;*/
-	/*int i = 0;
-	int count = 0;
-	for (i = 1; i <= 100; i++)
-	{
-		if (i%10 == 9)
-			count++;
-		if (i / 10 == 9)
-			count++;
-	}
-	printf("%d\n", count);
-	return 0;*/
-	/*int a = 0, b = 0;
-	for (a = 1, b = 1; a <= 100; a++)
-	{
-		if (b >= 20) break;
-		if (b % 3 == 1)
-		{
-			b = b + 3;
-			continue;
-		}
-		b = b - 5;
-	}
-	printf("%d\n", a);
-	return 0;*/
+	b = b - 5;
+}
+printf("%d\n", a);
+return 0;*/
 
