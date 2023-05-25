810673ac8fd5cc8a9bf3f5f5d3b07985bd76fb5b
ZaxonChow <saxon12@126.com>
2023-05-25 21:34:22 +0800

    RWFILE

--- 2023_05_25_RWFile/RWFile.c
+++ 2023_05_25_RWFile/RWFile.c
@@ -76,8 +76,91 @@
 //	return 0;
 //}
 
+
+//fscanf
+//struct S
+//{
+//	char arr[10];
+//	int age;
+//	float score;
+//};
+//int main()
+//{
+//	struct S s = { 0 };
+//	FILE* pf = fopen("text.txt", "r");
+//	if (pf == NULL)
+//	{
+//		perror("fopen");
+//		return 1;
+//	}
+//	//pf改成stdout，输出到屏幕
+//	fprintf(pf,"%s %d %f", s.arr,s.age,s.score);
+//	fscanf(pf, "%s %d %f", s.arr, &(s.age), &(s.score));
+//	printf("%s %d %f\n", s.arr, s.age, s.score);
+//
+//	fclose(pf);
+//
+//	return 0;
+//}
+
+//流
+//FILE*
+// 
+//printf
+//scanf
+//
+//任何一个c程序，只要运行，默认打开3个流
+//FILE* stdin  - 标准输入流（键盘）
+//FILE* stdout - 标准输出流（屏幕）
+//FILE* stderr - 标准错误流（屏幕）
+//
+
+//二进制输入输出
+//fread
+//fwrite
+//只针对文件
+
+struct S
+{
+	char arr[10];
+	int age;
+	float score;
+};
+
 int main()
 {
-
+	struct S s = { 0 };
+	//二进制方式读文件
+	FILE* pf = fopen("text.txt", "rb");
+	if (pf == NULL)
+	{
+		perror("fopen");
+		return 1;
+	}
+	//读
+	fread(&s, sizeof(struct S), 1, pf);
+	//读一个大小为sz的数据
+	printf("%s %d %f\n", s.arr, s.age, s.score);
+	fclose(pf);
+	pf = NULL;
 	return 0;
 }
+
+//int main()
+//{
+//	struct S s = { "zhangsan", 25, 50.5f };
+//	//二进制方式写文件
+//	FILE* pf = fopen("text.txt", "wb");
+//	if (pf == NULL)
+//	{
+//		perror("fopen");
+//		return 1;
+//	}
+//	//写
+//	fwrite(&s, sizeof(struct S), 1, pf);
+//	//写一个大小为sz的数据
+//
+//	fclose(pf);
+//	pf = NULL;
+//	return 0;
+//}
