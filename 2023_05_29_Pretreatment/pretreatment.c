b1f12e0fe1112764af8395001fa38c6d0448017e
ZaxonChow <saxon12@126.com>
2023-05-29 17:42:42 +0800

    pretreatment

---
+++ 2023_05_29_Pretreatment/pretreatment.c
@@ -0,0 +1,73 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <stdlib.h>
+
+//链接
+// 合并段表
+// 符号表的合并和重定位
+// 《程序员的自我修养》
+//运行环境
+// 程序载入内存中
+// 程序开始执行，调用main函数
+// 开始执行程序代码
+// 终止程序
+//预处理
+// 预定义符号
+// __FILE__  
+// __LINE__
+// __DATE__
+// __TIME__
+// 
+
+//int main()
+//{
+//	int i = 0;
+//	FILE* pf = fopen("log.txt", "w");
+//	if (pf == NULL)
+//	{
+//		perror("fopen");
+//		return EXIT_FAILURE;
+//	}
+//	for (i = 0; i < 10; i++)
+//	{
+//		fprintf(pf, "file:%s line=%d date:%s time:%s i=%d\n", __FILE__, __LINE__,__DATE__, __TIME__,i);
+//	}
+//	fclose(pf);
+//	pf = NULL;
+//	return 0;
+//}
+
+//int main()
+//{
+//	//printf("%d\n", __STDC__);
+//	return 0;
+//}
+
+//#define MAX 1000
+//#define STR "hello bit"
+//#define print printf("hehe\n")
+//
+//int main()
+//{
+//	int m = MAX;
+//	printf("%d\n", MAX);
+//	printf("%s\n", STR);
+//	print;
+//	return 0;
+//}
+
+//int main()
+//{
+//	int n = 0;
+//	switch (n)
+//	{
+//	case 1:
+//		break;
+//	case 2:
+//		break;
+//	case 3:
+//		break;
+//	}
+//	return 0;
+//}
+
